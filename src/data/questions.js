bfc0c7777302789204965867868799ca03ac58c9
Viviane Ferreira <99060121+viviferreiras@users.noreply.github.com>
2023-05-14 09:46:04 -0300

    subindo arrqivo para o git

---
+++ src/data/questions.js
@@ -0,0 +1,40 @@
+const data = [
+    {
+        category: "Literatura",
+        questions: [
+          {
+            question: "Quem é o autor de 'O principe'",
+            options: [
+              "Collen Hoover",
+              "Nicolau Maquiavel",
+              "Willian Shakespeare",
+              "José Saramago",
+            ],
+            answer: "Nicolau Maquiavel",
+          },
+          {
+            question: "De qual livro é essa frase: 'Tu te tornas eternamente responsável por aquilo que cativas'.",
+            options: ["Biblia Sagrada", "Hamelt", "Crime e Castigo", "O Pequeno Principe"],
+            answer: "O Pequeno Principe",
+          },
+          {
+            question: "Qual a obra mais importante de Machado de Assis?",
+            options: ["Memórias Póstumas de Brás Cubas", "Quincas Borba", "Dom Casmurro", "Helena"],
+            answer: "Dom Casmurro",
+          },
+          {
+            question: "As expressões Carpe diem, Locus amoenus, Fugere urbem, estão relacionadas diretamente a qual período literário?",
+            options: ["Arcadismo/Neoclassicismo", "Barroco", "Classicismo", "Romantismo"],
+            answer: "Arcadismo/Neoclassicismo",
+          },
+          {
+            question:
+              "Qual dos escritores escreveu a 'Divina Comédia'?",
+            options: ["Visconde de Taunay", "Dante Alighieri", "Machado de Assis", "Gil Vicente"],
+            answer: "Dante Alighieri",
+          },
+        ],
+      },
+];
+
+export default data;
---
+++ src/data/questions_complete.js
@@ -0,0 +1,90 @@
+const data = [
+    {
+      category: "The Big Bang theory",
+      questions: [
+        {
+          question: "Sheldon da spoiler sobre um livro para Leonard, qual livro?",
+          options: ["Jogos Vorazes", "Harry Potter", "Senhor dos aneis", "Percy Jackson"],
+          answer: "Harry Potter",
+        },
+        {
+          question: "Na noite de pizza, quando Priya anula o contrato de colegas de quarto, qual comida eles compram?",
+          options: ["Indiana", "Grega", "Japonesa", "Árabe"],
+          answer: "Grega",
+        },
+        {
+          question: "Qual a profissão da ex namorada de Raj, Emily?",
+          options: ["Dermatologista", "Esteticista", "Cozinheira", "Professora"],
+          answer: "Dermatologista",
+        },
+        {
+          question: "Qual camisa Sheldon usa nas sextas?",
+          options: ["Flash", "Lanterna Verde", "Capitão América", "Homem Aranha"],
+          answer: "Flash",
+        },
+        {
+          question: "Em qual jogo online Sheldon tem sua conta hackeada? (4ª temporada)",
+          options: ["Age Of Connar", "World Of Warcraft", "sRed Dead Redemption", "Zork"],
+          answer: "World Of Warcraft",
+        },
+      ],
+    },
+    {
+      category: "Conhecimentos Gerais",
+      questions: [
+        {
+          question: "Quais o menor e o maior país do mundo?",
+          options: ["Vaticano e Rússia", "Mônaco e Canadá", "São Marino e Índia", "Nauru e China"],
+          answer: "Vaticano e Rússia",
+        },
+        {
+          question: "Qual personagem folclórico costuma ser agradado pelos caçadores com a oferta de fumo?",
+          options: ["Lobisomem", "Negrinho do Pastoreio", "Saci", "Caipora"],
+          answer: "Caipora",
+        },
+        {
+          question: "As pessoas de qual tipo sanguíneo são consideradas doadores universais?",
+          options: ["Tipo A", "Tipo B", "Tipo O", "Tipo AB"],
+          answer: "Tipo O",
+        },
+      ],
+    },
+    {
+      category: "Literatura",
+      questions: [
+        {
+          question: "Quem é o autor de 'O principe'",
+          options: [
+            "Collen Hoover",
+            "Nicolau Maquiavel",
+            "Willian Shakespeare",
+            "José Saramago",
+          ],
+          answer: "Nicolau Maquiavel",
+        },
+        {
+          question: "De qual livro é essa frase: 'Tu te tornas eternamente responsável por aquilo que cativas'.",
+          options: ["Biblia Sagrada", "Hamelt", "Crime e Castigo", "O Pequeno Principe"],
+          answer: "O Pequeno Principe",
+        },
+        {
+          question: "Qual a obra mais importante de Machado de Assis?",
+          options: ["Memórias Póstumas de Brás Cubas", "Quincas Borba", "Dom Casmurro", "Helena"],
+          answer: "Dom Casmurro",
+        },
+        {
+          question: "As expressões Carpe diem, Locus amoenus, Fugere urbem, estão relacionadas diretamente a qual período literário?",
+          options: ["Arcadismo/Neoclassicismo", "Barroco", "Classicismo", "Romantismo"],
+          answer: "Arcadismo/Neoclassicismo",
+        },
+        {
+          question:
+            "Qual dos escritores escreveu a 'Divina Comédia'?",
+          options: ["Visconde de Taunay", "Dante Alighieri", "Machado de Assis", "Gil Vicente"],
+          answer: "Dante Alighieri",
+        },
+      ],
+    },
+  ];
+
+  export default data;
